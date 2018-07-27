1d61848d4bdec0b525a15629354fc92f05b73188
David Nicolle <davidanicolle@hotmail.com>
2018-07-27 17:57:10 +0000

    reconstruction.py

--- reconstruction.py
+++ reconstruction.py
@@ -1,1 +1,38 @@
-#this is the python file for reconstructing the protein sequence
+unscrambled = ""
+transition = ""
+finished = False
+size = 0
+scrambled = open("puzzle.txt").read()
+x=scrambled.replace(",", "").replace("\n", " ").split(" ")
+unscrambled += x[0]
+while not(finished):
+	finished = True
+	for i in range(14, 1, -1):
+		for fragment in x:
+			key = unscrambled[-i:]
+			if key in fragment:
+				if not(fragment in unscrambled):
+					size = 15 - i
+					unscrambled += fragment[-size:]
+					finished = False
+					break
+		if not(finished):
+			break
+print("Test")
+finished = False
+while not(finished):
+	finished = True
+	for i in range(14, 1, -1):
+		for fragment in x:
+			key = unscrambled[i:]
+			if key in fragment:
+				if not(fragment in unscrambled):
+					transition = fragment
+					size = len(unscrambled) - i
+					transition += unscrambled[-size:]
+					unscrambled = transition
+					finished = False
+					break
+		if not(finished):
+			break
+print(unscrambled)
