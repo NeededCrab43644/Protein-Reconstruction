ea2033ae780d6a27fd969dac650381ce0bb12c2b
David Nicolle <davidanicolle@hotmail.com>
2018-07-27 18:18:06 +0000

    reconstruction.py

--- reconstruction.py
+++ reconstruction.py
@@ -2,7 +2,7 @@ unscrambled = ""
 transition = ""
 finished = False
 size = 0
-scrambled = open("puzzle.txt").read()
+scrambled = open("puzzle2.txt").read()
 x=scrambled.replace(",", "").replace("\n", " ").split(" ")
 unscrambled += x[0]
 while not(finished):
@@ -24,7 +24,7 @@ while not(finished):
 	finished = True
 	for i in range(14, 1, -1):
 		for fragment in x:
-			key = unscrambled[i:]
+			key = unscrambled[0:i]
 			if key in fragment:
 				if not(fragment in unscrambled):
 					transition = fragment
